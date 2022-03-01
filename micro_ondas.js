7a6274196b439a1625d1993100dc00ce7ea4f141
KennethOlusegun <kennetholusegun@gmail.com>
2022-03-01 01:26:32 -0300

    checkpoint

--- micro_ondas.js
+++ micro_ondas.js
@@ -1,23 +1,64 @@
-let pipoca = "10 segundos"
+const pratos = {
+    1: { nome: 'Pipoca', tempo: 10 },
+    2: { nome: 'Macarrão', tempo: 8 },
+    3: { nome: 'Carne', tempo: 15 },
+    4: { nome: 'Feijão', tempo: 12 },
+    5: { nome: 'Brigadeiro', tempo: 8 },
+};
 
-function Menu(pipoca) {
-    if(!pipoca){
-        console.log("Prato Inexistente")
+function Menu(numeroPrato, tempo) {
+    const prato = pratos[numeroPrato];
+
+    if (!prato) {
+        return console.log('Prato inexistente');
     }
 
     let mensagem;
 
-    if(pipoca < 10){
-        mensagem = console.log("Tempo Insuficiente")
-    } else if(pipoca > 10 * 2 && pipoca <= 10 * 3){
-        mensagem = console.log("A comida queimou")
-    } else if(pipoca > 10 * 3){
-        mensagem = console.log("KABUMMMM!")
-    } else {
-        mensagem = console.log("Prato pronto, bom apetite!")
-    }
+    const tempoPadrao = prato.tempo;
 
-    console.log(mensagem)
+    let tempoContador = tempo;
+
+    if (tempo < tempoPadrao) {
+
+        mensagem = 'Tempo insuficiente';
+
+        console.log(mensagem);
+
+    } else if (tempo == (tempoPadrao * 3)) {
+        for (let i = 0; i < tempoContador; i++) {
+            let time = (1000 * i);
+            setTimeout(() => {
+                i++;
+                console.log(i);
+                if (i == tempoContador) {
+                    console.log('KABUMMM!!');
+                };
+            }, time)
+        }
+    } else if (tempo == (tempoPadrao * 2) && tempo < (tempoPadrao * 3)) {
+        for (let i = 0; i < tempoContador; i++) {
+            let time = (1000 * i);
+            setTimeout(() => {
+                i++;
+                console.log(i);
+                if (i == tempoContador) {
+                    console.log('A comida queimou');
+                };
+            }, time);
+        }
+    } else {
+        for (let i = 0; i < tempoContador; i++) {
+            let time = (1000 * i);
+            setTimeout(() => {
+                i++;
+                console.log(i);
+                if (i == tempoContador) {
+                    console.log('Prato pronto, bom apetite!!!');
+                };
+            }, time);
+        }
+    }
 }
 
-Menu(pipoca,100)
+Menu(2, 15)
